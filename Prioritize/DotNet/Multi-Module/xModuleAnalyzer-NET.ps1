f86b6098e2ef640eeca6378efdefebe68178751b
hugh-whitesource <77014894+hugh-whitesource@users.noreply.github.com>
2022-11-29 16:53:02 -0500

    Stopping point

---
+++ Prioritize/DotNet/Multi-Module/xModuleAnalyzer-NET.ps1
@@ -0,0 +1,11 @@
+Set-Variable SEARCHDIR (Get-Location)
+Set-Variable RELEASEDIR "*\bin\*"
+
+Get-ChildItem -Include "*.csproj" -Recurse | Where-Object DirectoryName -NotLike *build* | Where-Object DirectoryName -NotLike *test* | Where-Object DirectoryName -NotLike *host* | Where-Object DirectoryName -NotLike *migration* | ForEach-Object {
+     Write-Output "Found $_"
+     Set-Variable CSPROJ $_.BaseName
+     #Write-Output "Basename is $CSPROJ"
+     Get-ChildItem -Recurse -Include *.dll | Where-Object FullName -like $RELEASEDIR$CSPROJ.dll | ForEach-Object {
+        Write-Output $_.FullName >>multi-module.txt
+     }
+    }
