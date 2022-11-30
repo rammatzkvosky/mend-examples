58afde6a809e53e5651d3272d1b0093b65088340
hugh-whitesource <77014894+hugh-whitesource@users.noreply.github.com>
2022-11-30 16:53:44 -0500

    initial commit

--- Prioritize/DotNet/Multi-Module/xModuleAnalyzer-NET.ps1
+++ Prioritize/DotNet/Multi-Module/xModuleAnalyzer-NET.ps1
@@ -1,12 +1,20 @@
+Set-Variable JARFILE C:\Users\HughCushing\wss-unified-agent.jar
 Set-Variable SEARCHDIR (Get-Location)
-Set-Variable RELEASEDIR "*\bin\*"
 
-Get-ChildItem -Include "*.csproj" -Recurse | Where-Object DirectoryName -NotLike *build* | Where-Object DirectoryName -NotLike *test* | Where-Object DirectoryName -NotLike *host* | Where-Object DirectoryName -NotLike *migration* | ForEach-Object {
-     Write-Output "Found $_"
+if (Test-Path -Path .\multi_module.ps1 -PathType Leaf) {
+   Remove-Item .\multi_module.ps1
+}
+
+Get-ChildItem -Include "*.csproj" -Recurse | Where-Object DirectoryName -NotLike *build* | Where-Object DirectoryName -NotLike *test* | Where-Object DirectoryName -NotLike *host* | Where-Object DirectoryName -NotLike *migration*  | ForEach-Object {
+     Set-Variable CSPROJDIR (Resolve-Path $_ | Split-Path -parent)
+     Write-Output "CSPROJDIR= $CSPROJDIR"
      Set-Variable CSPROJ $_.BaseName
-     #Write-Output "Basename is $CSPROJ"
-     Get-ChildItem -Recurse -Include *.dll | Where-Object FullName -like $RELEASEDIR$CSPROJ.dll | ForEach-Object {
-        Write-Output $_.FullName >>multi-module.txt
+     Get-ChildItem -Recurse -Include *.dll | Where-Object FullName -like $CSPROJDIR*\$CSPROJ.dll | Where-Object DirectoryName -NotLike *\obj\* | ForEach-Object {
+        #Set-Variable DLL $_.FullName
+        Set-Variable DLL (Resolve-Path -Relative $_)
+        Write-Output "DLL= $DLL"
+        Set-Variable RELCSPROJDIR (Resolve-Path -Relative $CSPROJDIR)
+        Write-Output "RELCSPROJDIR= $RELCSPROJDIR"
+        Write-Output "java -XX:+UseG1GC -Xmx8g -jar $JARFILE -appPath $DLL -d $RELCSPROJDIR -project $CSPROJ" >>multi_module.ps1
      }
-    }
-    
+   }
